adcd07d82eed5b55fee419d3c5e0372a07b15ad5
Nilesh Kumar <nileshkumar12521@gmail.com>
2026-07-28 00:36:25 +0530

    working

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { use, useEffect } from 'react'
+import React, { use, useEffect, useState } from 'react'
 import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import TaskNumber from './components/others/TaskNumber'
@@ -7,14 +7,26 @@ import { getLocalStorage, setLocalStorage } from './utils/LocalStorage'
 
 const App = () => {
 
-  useEffect(()=>{
-    // setLocalStorage()
-    getLocalStorage()
-  },)
+  const [user, setUser] = useState(null)
+
+  const handleLogin = (email , password)=>{
+    if(email == "admin@admin.com" && password == '123'){
+      console.log("this is admmin")
+    }
+    else if(email == "employees@employees.com" && password == '123'){
+        console.log("this is Employees")
+    }
+    else {
+      alert("Invalid Ceadintials")
+    }
+  }
+
+  handleLogin('admin@admin.com' , 123)
 
   return (
     <>
-    <Login />
+    {/* it means if there will be user then nohing will display and if there will be not a user then the login page eill appear */}
+    { user ?  '' :<Login handelLLogin={handleLogin}/>  }  
     {/* <EmployeeDashboard /> */}
     {/* <AdminDashboard /> */}
     </>
--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
 
-const Login = () => {
+const Login = ({handelLogin}) => {
 
+  console.log(handelLogin)
+  
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
