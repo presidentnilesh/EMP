872178a888a7f4283e9d6efe2c86ec58445a44ea
Nilesh Kumar <nileshkumar12521@gmail.com>
2026-08-05 18:24:30 +0530

    deleted authcontext

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
-import { StrictMode } from 'react'
+import { StrictMode, useContext } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import AuthCOntext from './context/AuthCOntext.jsx'
-import TaskContext from './context/TaskContext.jsx'
+import AuthProvider from './context/AuthProvider.jsx'
 
 
 createRoot(document.getElementById('root')).render(
-    <AuthCOntext >
-      <TaskContext >
-        <App />
-      </TaskContext>
-    </AuthCOntext>
+  <AuthProvider >
+    <App />
+  </AuthProvider>
   
 )
