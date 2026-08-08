0de90b30fc9bcdae9118fa7d4932720aa80f8282
Nilesh Kumar <nileshkumar12521@gmail.com>
2026-08-09 04:09:32 +0530

    fixing the issue and data.firstName is not displaying

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,39 @@
-import React, { use, useContext, useEffect, useState } from 'react'
+import React, { use, useContext, useDebugValue, useEffect, useState } from 'react'
 import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import TaskNumber from './components/others/TaskNumber'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
-import { getLocalStorage, setLocalStorage } from './utils/LocalStorage'
+import { admin, employees, getLocalStorage, setLocalStorage } from './utils/LocalStorage'
 import { AuthContext } from './context/AuthProvider'
 
 const App = () => {
 
   const [user, setUser] = useState(null)
+  const [loggedInUserData, setLoggedInUserData] = useState(null)
   const authData = useContext(AuthContext)
+
+  // useEffect(() => {
+  //     if(authData){
+  //       const loggedInUser = localStorage.getItem('loggedInUser')
+  //       setUser(loggedInUser.role)
+  //     }
+  // }, [authData])
+  
 
 
   const handleLogin = (email , password)=>{
     if(AuthContext && authData?.admin.find((e)=>email==e.email && password==e.password )){
       setUser("Admin")
-
+      setLoggedInUserData(adminUser)
+      localStorage.setItem('loggedInUser',JSON.stringify({role:"Admin" , user:adminUser} ))
     }
-    else if(AuthContext && authData?.employees.find((e)=>email == e.email && password == password )){
-      setUser('Employee')
+    else if(AuthContext){
+      const employee = authData?.employees.find((e)=>email == e.email && password == e.password)
+      if (employees){
+        setUser('Employee')
+        setLoggedInUserData(employees)
+        localStorage.setItem('loggedInUser',JSON.stringify({role:"Employees" }))
+      }
     }
     else {
       alert("Invalid Ceadintials")
@@ -33,9 +48,9 @@ const App = () => {
     {/* it means if there will be user then nohing will display and if there will be not a user then the login page eill appear */}
     {!user ? ( <Login handelLogin={handleLogin} /> ) : '' }
     {user ? (
-      user === "Admin" ? <AdminDashboard /> : <EmployeeDashboard />
+      user === "Admin" ? <AdminDashboard /> : <EmployeeDashboard data={loggedInUserData}/>
     ) : ''}
-    {/* <EmployeeDashboard /> */}
+    {/* <EmployeeDashboard data={loggedInUserData/> */}
     {/* <AdminDashboard /> */}
     </>
   )
