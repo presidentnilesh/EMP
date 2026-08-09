d75158e195aa46d1534e0f7fcbf1fc78ec1a50b9
Nilesh Kumar <nileshkumar12521@gmail.com>
2026-08-09 08:11:27 +0530

    made dedicated file/component for all tasks

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -4,16 +4,27 @@ import NewTask from './NewTask'
 import FailedTask from './FailedTask'
 import CompleteTask from './CompleteTask'
 
-const TaskList = () => {
+const TaskList = ({data}) => {
+  console.log(data)
+
   return (
     <div id='tasklist' className='flex overflow-x-auto justify-self-start items-center gap-10 flex-nowrap w-full h-[55%] mt-10 py-5'>
         
-        <AcceptTask />
-        <NewTask />
-        <CompleteTask />
-        <FailedTask />
-        
-        
+      {data.tasks.map((elem)=>{
+        if(elem.active){
+          return <AcceptTask />
+        }
+        if (elem.completed){
+          return <CompleteTask />
+        }
+        if (elem.failed){
+          return <FailedTask />
+        }
+        if (elem.newTask){
+          return <NewTask  />
+        }
+      })}
+
     </div>
   )
 }
